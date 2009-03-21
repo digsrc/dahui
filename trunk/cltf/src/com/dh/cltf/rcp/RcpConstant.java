9c34be827e3c9f03d8e558dd5e109d7b14c39d14
dhyuan <dhyuan@d91aabfe-dfb1-11dd-bd7b-b12b6590754f>
2009-03-21 08:35:31 +0000

    move the rcp code into the rcp package.

---
+++ trunk/cltf/src/com/dh/cltf/rcp/RcpConstant.java
@@ -0,0 +1,9 @@
+package com.dh.cltf.rcp;
+
+public class RcpConstant {
+
+	public static final String CONSOLE_PERSPECTIVE_ID = "com.dh.cltf.rcp.app.ConsolePerspective";
+	
+	public static final String NAVIGATOR_VIEW_ID = "com.dh.cltf.rcp.app.view.NavigatorView";
+	public static final String DEVICE_CONSOLE_VIEW_ID = "com.dh.cltf.rcp.app.view.DeviceConsoleView";
+}
--- trunk/cltf/src/com/dh/cltf/rcp/app/perspective/ConsolePerspective.java
+++ trunk/cltf/src/com/dh/cltf/rcp/app/perspective/ConsolePerspective.java
@@ -4,7 +4,7 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
-import com.dh.cltf.rcp.app.Constant;
+import com.dh.cltf.rcp.RcpConstant;
 
 public class ConsolePerspective implements IPerspectiveFactory {
 
@@ -12,9 +12,9 @@ public class ConsolePerspective implements IPerspectiveFactory {
 		String editorArea = layout.getEditorArea();
 		
 		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.3f, editorArea);
-		left.addView(Constant.NAVIGATOR_VIEW_ID);
+		left.addView(RcpConstant.NAVIGATOR_VIEW_ID);
 		
 		IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.5f, "left");
-		bottom.addView(Constant.DEVICE_CONSOLE_VIEW_ID);
+		bottom.addView(RcpConstant.DEVICE_CONSOLE_VIEW_ID);
 	}
 }
