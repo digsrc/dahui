fa83b2ae9c7e549f36fc0d76452055c31a410b1e
dhyuan <dhyuan@d91aabfe-dfb1-11dd-bd7b-b12b6590754f>
2009-04-27 14:26:45 +0000

    tcl command/file executor

---
+++ trunk/cltf/testSrc/com/dh/cltf/fw/tcl/TestTclExecutor.java
@@ -0,0 +1,54 @@
+package com.dh.cltf.fw.tcl;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Test case for TclExecutor.
+ * 
+ * @author Yuan Dahui.
+ *
+ */
+public class TestTclExecutor {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+	}
+	
+	/**
+	 * Test stop the TCL interpreter.
+	 * 
+	 * @throws InterruptedException exception
+	 */
+	@Test
+	public void testStopTclExecutor() throws InterruptedException {
+		TclExecutor executorA = TclExecutor.getTclExecutor("TclInterpreter-A");
+		TclExecutor executorB = TclExecutor.getTclExecutor("TclInterpreter-B");
+		
+		boolean isSameNotifier = 
+			executorA.getTclInterpreter().getNotifier() == executorB.getTclInterpreter().getNotifier();
+		// Each TclExecutor's interpreter thread has its own notifier object.
+		assertTrue(! isSameNotifier);
+		
+		assertTrue(executorA.isTclInterpClosed());
+		assertTrue(executorB.isTclInterpClosed());
+		
+		executorA.dispose();
+		executorB.dispose();
+		
+		// TCL intperpreter's notifier's doOneEvent() is blocked if no event to be processed.
+		// so, we feed it with a event.
+		executorA.execute("puts \"close me A\"");
+		executorB.execute("puts \"close me B\"");
+		
+		
+		// TCL interpreter runs in another thread, so we need some time to wait it finishing stop.
+		final int sleepTime = 200;
+		Thread.sleep(sleepTime);
+		
+		assertTrue(!executorA.isTclInterpClosed());
+		assertTrue(!executorB.isTclInterpClosed());
+	}
+}
